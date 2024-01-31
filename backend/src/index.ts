3fc07afe7d81cb97efae15b142629d6a1ead1b08
philip bein <philipbein10697@gmail.com>
2024-01-30 20:01:41 -0500

    deleted old code and started over fresh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import * as RecipeAPI from './recipe-api';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 app.get("/api/recipes/search", async (req, res) => {
-    const searchTerm = req.query.searchTerm as string;
-    const page = parseInt(req.query.page as string);
-    const results = await RecipeAPI.searchRecipes(searchTerm, page);
+    res.json({ message: "success!" })     
+})
 
-    return res.json(results);
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
 });
-
-app.listen(5000, () => console.log('Server running on port 5000'));
-
-
