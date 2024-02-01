cdbc4966e39e3f4f4c9bbce2aaf766e92403cc3b
philip bein <philipbein10697@gmail.com>
2024-01-31 20:21:49 -0500

    added the app.ts file and imported it into the App.tsx file as api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
 import './App.css'
+import { useState } from 'react'
+import * as api from './api'
 
 const App = () => {
+const [searchTerm, setSearchTerm] = useState("burgers");
+const [recipes, setRecipes] = useState([]);
+
+const handleSearchSubmit = async () => {
+  try {
+    const recipes = await api.searchRecipes(searchTerm, 1)
+  } catch (e) {
+    console.log(e);
+  }
+}
+
   return(
     <div>Hello from Recipe App!</div>
   )
