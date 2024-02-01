b57d805fd5b4266d7c638b402272ece2221af5bd
philip bein <philipbein10697@gmail.com>
2024-01-31 20:34:59 -0500

    added the recipe mapping to test multiple recipes from a search to test on the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,34 @@
-import './App.css'
-import { useState } from 'react'
-import * as api from './api'
+import "./App.css";
+import { useState } from "react";
+import * as api from "./api";
+import { Recipe } from "./types";
 
 const App = () => {
-const [searchTerm, setSearchTerm] = useState("burgers");
-const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("burgers");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-const handleSearchSubmit = async () => {
-  try {
-    const recipes = await api.searchRecipes(searchTerm, 1)
-  } catch (e) {
-    console.log(e);
-  }
-}
+  const handleSearchSubmit = async () => {
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  return(
-    <div>Hello from Recipe App!</div>
-  )
+  return (
+    <div>
+      <form onSubmit={() => handleSearchSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+      {recipes.map((recipe) => (
+        <div>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
