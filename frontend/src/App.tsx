f1b14ccc162631cfc1f03e06ff94c1a179883f6c
philip bein <philipbein10697@gmail.com>
2024-02-01 22:38:22 -0500

    Added the RecipeCard and the components folder in order to create a object that can show the image of the food and have it mapped over every recipe to be displayed on the screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import * as api from "./api";
 import { Recipe } from "./types";
+import RecipeCard from "./components/RecipeCard";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("burgers");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-  const handleSearchSubmit = async () => {
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
     try {
       const recipes = await api.searchRecipes(searchTerm, 1);
-      setRecipes(recipes);
+      setRecipes(recipes.results);
     } catch (e) {
       console.log(e);
     }
@@ -18,14 +20,19 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={() => handleSearchSubmit}>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <input 
+        type="text" 
+        required 
+        placeholder="Enter a search term ..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+        />
+
         <button type="submit">Submit</button>
       </form>
       {recipes.map((recipe) => (
-        <div>
-          recipe image location: {recipe.image}
-          recipe title: {recipe.title}
-        </div>
+        <RecipeCard recipe={recipe}/>
       ))}
     </div>
   );
